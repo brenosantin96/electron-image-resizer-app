09c09ba7067c87b05deb5d2f2f3c7b83311f9c4b
Breno Santin <brenolimaa96@gmail.com>
2024-10-06 19:21:00 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,109 @@
+const path = require("path");
+const { app, BrowserWindow, Menu, globalShortcut } = require("electron")
+console.log("Hello World");
+
+const isDev = process.env.NODE_ENV !== "production"
+const isMac = process.platform === "darwin";
+
+
+//Create mainWindow
+function createMainWindow() {
+
+    const mainWindow = new BrowserWindow({
+        title: "Image Resizer",
+        width: isDev ? 1000 : 500,
+        height: 800
+    });
+
+    //open devtools if in dev env
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
+
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+
+}
+
+//Create about window
+function createAboutWindow() {
+    const aboutWindow = new BrowserWindow({
+        title: "About Image Resizer",
+        width: 300,
+        height: 300
+    });
+
+    aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'));
+}
+
+
+//App is ready
+app.whenReady().then(() => {
+
+    // Register a 'CommandOrControl+X' shortcut listener.
+    const ret = globalShortcut.register('CommandOrControl+Q', () => {
+        console.log('CommandOrControl+Q is pressed')
+    })
+
+    //Implement Menu
+    const mainMenu = Menu.buildFromTemplate(menu)
+    Menu.setApplicationMenu(mainMenu);
+
+
+    if (!ret) {
+        console.log('registration failed')
+    }
+
+    createMainWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow();
+        }
+    })
+})
+
+//Menu Template
+const menu = [
+
+    ...(isMac ? [{
+        label: app.name,
+        submenu: [
+            {
+                label: "About",
+                click: createAboutWindow,
+            }
+        ]
+    }] : []),
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: "Quit",
+                click: () => app.quit()
+            }
+        ]
+    },
+    ...(!isMac ? [{
+        label: "Help",
+        submenu: [
+            {
+                label: "About",
+                click: createAboutWindow,
+            }
+        ]
+    }] : [])
+]
+
+app.on("window-all-closed", () => {
+    if (!isMac) {
+        app.quit();
+    }
+})
+
+app.on('will-quit', () => {
+    // Unregister a shortcut.
+    globalShortcut.unregister('CommandOrControl+Q')
+
+    // Unregister all shortcuts.
+    globalShortcut.unregisterAll()
+})
